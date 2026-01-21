f4fb7cd4c738c9fed7f532c74804bda7fb5bb2cd
9esTerem <9esTerem@ktch.hu>
2026-01-21 19:49:37 +0100

    book_extended

--- KönyvtárKezelő/Book.cs
+++ KönyvtárKezelő/Book.cs
@@ -9,5 +9,13 @@ namespace KönyvtárKezelő
     public class Book
     {
 
+        public string ISBN { get; set; }        
+        public string Title { get; set; }       
+        public string Author { get; set; }      
+        public int Year { get; set; }            
+        public bool IsAvailable { get; set; }    
+
+
+
     }
 }
